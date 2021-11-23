cb4827e03b81249bbf6389d631ec21dd7dcdbef2
Joshua Salinas <joshuasalinashernandez@gmail.com>
2021-11-22 19:17:24 -0600

    Signleton to Factory Design

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintStudentDetails();
+            PrintEmployeeDetails();
+
+            Console.WriteLine("-------------------------------------");
+
+            Singleton.DerivedSingleton derivedObj = new Singleton.DerivedSingleton();
+            derivedObj.PrintDetails("From Derived");
+
+            Console.ReadLine();
+        }
+
+        private static void PrintEmployeeDetails()
+        {
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintDetails("From Employee");
+        }
+
+        private static void PrintStudentDetails()
+        {
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+        }
+    }
+}
---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SingletonDemo
+{
+    public class Singleton
+    {
+        private static int counter = 0;
+        private static object obj = new object();
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+        private static Singleton instance = null;
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public class DerivedSingleton : Singleton
+        {
+
+        }
+    }
+}
